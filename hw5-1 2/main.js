d8dd9c713d8454add711031391e333e1d489fe29
Denis Tomazzi <41800706+denistomazzi@users.noreply.github.com>
2020-09-06 20:38:43 -0500

    Add files via upload

---
+++ hw5-1 2/main.js
@@ -0,0 +1,17 @@
+$('.show-filters').on("click", function(){
+  if ($('.filters-list').is(':visible')){
+    $('.filters-list').slideUp()
+    $('.show-filters').text('Show filters')
+  } else {
+    $('.filters-list').slideDown()
+    $('.show-filters').text('Hide filters')
+  }
+  return false
+})
+$('.filters-list a').on('click', function (){
+     var filter = $(this).attr('data-filter')  
+     console.log(filter)
+     $('.item').hide()
+     $(filter).show()
+     return false
+     })
